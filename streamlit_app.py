05536bbd1062584fcd5c11cd4e44c4ef7a540ea8
ddeep9177 <127389602+ddeep9177@users.noreply.github.com>
2023-03-12 19:34:10 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,7 @@ streamlit.title('I will have healthy dinner')
 streamlit.header('todays menu')
 streamlit.text('🥣 rice')
 streamlit.text('🥗chole')
+
+streamlit.title('Build your own smoothy')
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
