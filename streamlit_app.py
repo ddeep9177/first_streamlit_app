d5af8ded72fc7a659b9d258b7d19f8f6029ada4b
ddeep9177 <127389602+ddeep9177@users.noreply.github.com>
2023-03-12 13:36:41 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit
 
 streamlit.title('I will have healthy dinner')
 streamlit.header('todays menu')
-streamlitm.text('rice')
-streamlitm.text('chole')
+streamlit.text('rice')
+streamlit.text('chole')
